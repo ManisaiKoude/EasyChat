d0ecbda4cec59d55ceb218a7b520aa4f73f06835
ManisaiKoude <koudemanisai@gmail.com>
2024-02-08 02:16:26 -0500

    Final commit

---
+++ app/src/main/java/com/example/easychat/LoginusernameActivity.java
@@ -0,0 +1,13 @@
+package com.example.easychat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginusernameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loginusername);
+    }
+}
